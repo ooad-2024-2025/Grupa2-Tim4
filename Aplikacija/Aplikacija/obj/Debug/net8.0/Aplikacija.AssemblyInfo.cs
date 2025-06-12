18491fa228edbb9ed4c77a926f408f6ecc9195bc
Hamza2etf <hkovac2@etf.unsa.ba>
2025-06-12 17:25:52 +0200

    treninzi
    treninzi

--- Aplikacija/Aplikacija/obj/Debug/net8.0/Aplikacija.AssemblyInfo.cs
+++ Aplikacija/Aplikacija/obj/Debug/net8.0/Aplikacija.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aplikacija")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3627cdb1a6c7c39d51fd519b6cfa0e7467eec067")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9514ae5c916d3260a055ec80a17bbc6a3959f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aplikacija")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aplikacija")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
